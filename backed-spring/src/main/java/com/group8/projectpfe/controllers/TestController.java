d31f9b9ba0fee1ccfae6ed0be137e5b981dd84de
aymanebiti <aymanbiti2@gmail.com>
2023-11-23 23:16:19 +0100

    add test

--- backed-spring/src/main/java/com/group8/projectpfe/controllers/TestController.java
+++ backed-spring/src/main/java/com/group8/projectpfe/controllers/TestController.java
@@ -1,11 +1,13 @@
 package com.group8.projectpfe.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1")
 public class TestController {
+    @GetMapping
     public String getName(){
         return "hello world";
     }
--- backed-spring/src/test/java/com/group8/projectpfe/ProjectPfeApplicationTests.java
+++ backed-spring/src/test/java/com/group8/projectpfe/ProjectPfeApplicationTests.java
@@ -5,27 +5,34 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultHandler;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-
-@WebMvcTest(TestController.class)
 @AutoConfigureMockMvc
+@WebMvcTest(TestController.class)
 class ProjectPfeApplicationTests {
-
-
 	@Autowired
 	MockMvc mockMvc;
 
-	@Test
-	public void testGetName() throws Exception {
+	@Autowired
+	private TestController controller;
 
+	@Test
+	void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
+	}
+	@Test
+	public void shouldReturnHelloWorld() throws Exception {
 		mockMvc.perform(get("/api/v1"))
 				.andExpect(status().isOk())
 				.andExpect(content().string("hello world"));
-
 	}
 
 }
