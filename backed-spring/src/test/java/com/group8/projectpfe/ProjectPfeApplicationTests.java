abbd46d2e3bb29e8a26cef8a50af4b607fb16b75
aymanebiti <aymanbiti2@gmail.com>
2023-11-23 22:05:55 +0100

    feat

--- backed-spring/src/test/java/com/group8/projectpfe/ProjectPfeApplicationTests.java
+++ backed-spring/src/test/java/com/group8/projectpfe/ProjectPfeApplicationTests.java
@@ -1,17 +1,31 @@
 package com.group8.projectpfe;
 
+import com.group8.projectpfe.controllers.TestController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+
+@WebMvcTest(TestController.class)
+@AutoConfigureMockMvc
 class ProjectPfeApplicationTests {
 
 
-	@Test
-	void contextLoads() {
+	@Autowired
+	MockMvc mockMvc;
 
-    }
+	@Test
+	public void testGetName() throws Exception {
+
+		mockMvc.perform(get("/api/v1"))
+				.andExpect(status().isOk())
+				.andExpect(content().string("hello world"));
+
+	}
 
 }
