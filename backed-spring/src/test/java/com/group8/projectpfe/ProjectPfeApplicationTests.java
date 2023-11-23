89f5009d1af9fbba3c352d0969b35fe0bb551306
aymanebiti <aymanbiti2@gmail.com>
2023-11-23 21:32:51 +0100

    feat

--- backed-spring/src/test/java/com/group8/projectpfe/ProjectPfeApplicationTests.java
+++ backed-spring/src/test/java/com/group8/projectpfe/ProjectPfeApplicationTests.java
@@ -1,14 +1,17 @@
 package com.group8.projectpfe;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 class ProjectPfeApplicationTests {
 
-//	@Test
-//	void contextLoads() {
-//
-//	}
+
+	@Test
+	void contextLoads() {
+
+    }
 
 }
