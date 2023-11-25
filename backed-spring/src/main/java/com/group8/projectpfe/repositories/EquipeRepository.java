0f41fbd104fffa82069195dff613f3b7b5dcb9c4
aymanebiti <aymanbiti2@gmail.com>
2023-11-25 01:19:41 +0100

    feat

--- backed-spring/src/main/java/com/group8/projectpfe/repositories/EquipeRepository.java
+++ backed-spring/src/main/java/com/group8/projectpfe/repositories/EquipeRepository.java
@@ -4,6 +4,6 @@ import com.group8.projectpfe.entities.Equipe;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-//@Repository
-//public interface EquipeRepository extends JpaRepository<Equipe, Integer> {
-//}
+@Repository
+public interface EquipeRepository extends JpaRepository<Equipe, Integer> {
+}
