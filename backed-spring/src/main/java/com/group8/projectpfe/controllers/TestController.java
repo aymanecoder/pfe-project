c20950da540cc3290fe04a679bc5e6181c8ea924
aymanebiti <aymanbiti2@gmail.com>
2023-11-25 01:18:17 +0100

    feat

--- backed-spring/src/main/java/com/group8/projectpfe/controllers/TestController.java
+++ backed-spring/src/main/java/com/group8/projectpfe/controllers/TestController.java
@@ -1,7 +1,7 @@
 package com.group8.projectpfe.controllers;
 
 import com.group8.projectpfe.entities.Equipe;
-import com.group8.projectpfe.repositories.EquipeRepository;
+//import com.group8.projectpfe.repositories.EquipeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,18 +11,18 @@ import java.util.List;
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class TestController {
-    private final EquipeRepository equipeRepository;
+//    private final EquipeRepository equipeRepository;
     @GetMapping
     public String getName(){
         return "hello world";
     }
-    @GetMapping("/equipes")
-    public List<Equipe> getEquipes(){
-        return equipeRepository.findAll();
-    }
-    @PostMapping("/add")
-    public Equipe addEquipe(@RequestBody Equipe equipe){
-        return equipeRepository.save(equipe);
+//    @GetMapping("/equipes")
+//    public List<Equipe> getEquipes(){
+//        return equipeRepository.findAll();
+//    }
+//    @PostMapping("/add")
+//    public Equipe addEquipe(@RequestBody Equipe equipe){
+//        return equipeRepository.save(equipe);
 
-    }
+//    }
 }
