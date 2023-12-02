e91b60893599d3e5076ca46421e1426e800634c3
aymanebiti <aymanbiti2@gmail.com>
2023-12-02 14:30:34 +0100

    add

--- backed-spring/src/test/java/com/group8/projectpfe/ProjectPfeApplicationTests.java
+++ backed-spring/src/test/java/com/group8/projectpfe/ProjectPfeApplicationTests.java
@@ -1,35 +1,18 @@
 package com.group8.projectpfe;
 
-import com.group8.projectpfe.controllers.TestController;
-
-
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultHandler;
-import org.springframework.test.web.servlet.ResultMatcher;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 class ProjectPfeApplicationTests {
 
 
-	@Autowired
-	private TestController controller;
 
-
-	@Test
-	void contextLoads() throws Exception {
-		assertThat(controller).isNotNull();
-	}
 
 
 
