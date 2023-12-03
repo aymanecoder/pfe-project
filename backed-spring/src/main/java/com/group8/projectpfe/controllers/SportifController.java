a3e6e1c1f6456fecd9f6c52aa81180a9c73e3bca
yassine <Oussiyassine1@gmail.com>
2023-12-03 12:37:34 +0100

    implement update sportif

--- backed-spring/src/main/java/com/group8/projectpfe/controllers/SportifController.java
+++ backed-spring/src/main/java/com/group8/projectpfe/controllers/SportifController.java
@@ -51,5 +51,27 @@ private final JwtService jwtService;
         return new ResponseEntity<>("User deleted successfully", HttpStatus.OK);
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateSportif(@PathVariable Integer id, @RequestBody SportifDTO sportifDTO) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null && authentication.isAuthenticated()) {
+            User user = (User) authentication.getPrincipal(); // Retrieve the authenticated user
+            SportifDTO existingSportif = sportifService.getSportifById(Long.valueOf(id));
+
+            if (existingSportif != null && existingSportif.getId().equals(user.getId())) {
+                // Check if the sportif to be updated belongs to the authenticated user
+                sportifDTO.setId(id); // Set the ID in the DTO
+                sportifService.updateSportif(sportifDTO);
+                return new ResponseEntity<>("Sportif updated successfully", HttpStatus.OK);
+            } else {
+                // Handle unauthorized update or sportif not found
+                return new ResponseEntity<>("Unauthorized update or sportif not found", HttpStatus.NOT_FOUND);
+            }
+        } else {
+            // Handle cases where the user is not authenticated
+            return new ResponseEntity<>("User not authenticated", HttpStatus.UNAUTHORIZED);
+        }
+    }
 
 }
