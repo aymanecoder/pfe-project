ac90b139ec3a0a131677af08978d404c92be78e9
yassine <Oussiyassine1@gmail.com>
2023-12-03 12:43:49 +0100

    fix email uniqueness

--- backed-spring/src/main/java/com/group8/projectpfe/entities/User.java
+++ backed-spring/src/main/java/com/group8/projectpfe/entities/User.java
@@ -26,6 +26,7 @@ public class User implements UserDetails {
     private String firstName;
     private String lastName;
     @Email
+    @Column(unique = true)
     private String email;
     private String password;
     private String address;
