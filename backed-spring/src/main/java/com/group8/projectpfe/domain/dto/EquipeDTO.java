e8d4bb204ab12cd80288cba0cf5e4d9368b57366
soukaini adil <adilelwerdi81@gmail.com>
2023-12-10 17:20:19 +0100

    create the class EquipeDTO

---
+++ backed-spring/src/main/java/com/group8/projectpfe/domain/dto/EquipeDTO.java
@@ -0,0 +1,19 @@
+package com.group8.projectpfe.domain.dto;
+
+import lombok.*;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Builder
+public class EquipeDTO {
+    private Integer id;
+    private SportifDTO admin;
+    private List<SportifDTO> members;
+    private byte[] logo; // Assuming a byte array for storing the image (you can use Blob in PostgreSQL)
+    private String description;
+    private String typeDeSport;
+}
