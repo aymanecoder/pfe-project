57e05d3fee6c60e382d6f7da864f87aa0710dda8
yassine <Oussiyassine1@gmail.com>
2023-12-17 00:35:16 +0100

    implement createTeam of TeamController

--- backed-spring/src/main/java/com/group8/projectpfe/entities/Sport.java
+++ backed-spring/src/main/java/com/group8/projectpfe/entities/Sport.java
@@ -1,6 +1,8 @@
 package com.group8.projectpfe.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,6 +16,7 @@ import lombok.NoArgsConstructor;
 @Builder
 public class Sport {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String description;
