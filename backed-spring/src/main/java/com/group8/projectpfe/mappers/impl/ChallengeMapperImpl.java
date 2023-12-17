decad8ff828ba96526abd31e3bca3d21335da9a3
yassine <Oussiyassine1@gmail.com>
2023-12-17 16:03:05 +0100

    implement challenege mapper and team mapper

--- backed-spring/src/main/java/com/group8/projectpfe/mappers/impl/ChallengeMapperImpl.java
+++ backed-spring/src/main/java/com/group8/projectpfe/mappers/impl/ChallengeMapperImpl.java
@@ -1,25 +1,57 @@
 package com.group8.projectpfe.mappers.impl;
 
 import com.group8.projectpfe.domain.dto.ChallengeDTO;
+import com.group8.projectpfe.domain.dto.SportDTO;
+import com.group8.projectpfe.domain.dto.SportifDTO;
+import com.group8.projectpfe.domain.dto.TeamDTO;
 import com.group8.projectpfe.entities.Challenge;
+import com.group8.projectpfe.entities.Sport;
+import com.group8.projectpfe.entities.Team;
+import com.group8.projectpfe.entities.User;
 import com.group8.projectpfe.mappers.Mapper;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
 public class ChallengeMapperImpl implements Mapper<Challenge, ChallengeDTO> {
 
     private final ModelMapper modelMapper;
-
+    private final TeamMapperImpl teamMapper;
+    private final SportMapperImpl sportMapper;
     @Override
     public ChallengeDTO mapTo(Challenge challenge) {
-        return modelMapper.map(challenge, ChallengeDTO.class);
+
+        ChallengeDTO challengeDTO= modelMapper.map(challenge, ChallengeDTO.class);
+
+        SportDTO sportDTO=sportMapper.mapTo(team.getSport());
+        List<Team> teamList = challenge.getTeams();
+        List<TeamDTO> teamDTOList = teamList.stream()
+                .map(teamMapper::mapTo)
+                .collect(Collectors.toList());
+
+        challengeDTO.setSport(sportDTO);
+        challengeDTO.setTeams(teamDTOList);
+        return challengeDTO;
     }
 
     @Override
     public Challenge mapFrom(ChallengeDTO challengeDTO) {
-        return modelMapper.map(challengeDTO, Challenge.class);
+
+        Challenge challenge=modelMapper.map(challengeDTO, Challenge.class);
+
+        List<TeamDTO> teamDTOList = challengeDTO.getTeams();
+        List<Team> teamList = teamDTOList.stream()
+                .map(teamMapper::mapFrom)
+                .collect(Collectors.toList());
+
+        Sport sport=sportMapper.mapFrom(challengeDTO.getSport());
+        challenge.setSport(sport);
+        challenge.setTeams(teamList);
+        return challenge;
     }
 }
